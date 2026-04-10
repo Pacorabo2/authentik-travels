9e6bf0b6593e3baee21afecf363d54cd838b1066
Nicolas Plaza <pacorabo2@hotmail.com>
2026-04-10 11:17:44 +0200

    Clean : changing margin on desc text on GroupTripDetailPage

--- app/(marketing)/groupTrip/[slug]/page.tsx
+++ app/(marketing)/groupTrip/[slug]/page.tsx
@@ -43,7 +43,7 @@ export default async function GroupTripDetailPage({
       </section>
 
       {/* INFOS CLÉS & PRÉSENTATION */}
-      <section className="container mx-auto py-20 px-6 grid grid-cols-1 lg:grid-cols-3 gap-16">
+      <section className="container mx-auto py-20 px-10 grid grid-cols-1 lg:grid-cols-3 gap-16">
         <div className="lg:col-span-2 space-y-8">
           <div className="flex gap-4 items-center">
             <div className="bg-amber-500 text-white px-4 py-1 rounded-full text-[12px] font-black uppercase">
