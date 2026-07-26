f3b3f250275c55de91ce2e63ffc5f691cf6fecf0
Nicolas Plaza <pacorabo2@hotmail.com>
2026-07-26 23:13:57 +0200

    Feat : Adding token on admin to protect

--- app/api/create-admin/route.ts
+++ app/api/create-admin/route.ts
@@ -2,8 +2,14 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
-export async function GET() {
+export async function GET(req: Request) {
   try {
+    const { searchParams } = new URL(req.url);
+    const token = searchParams.get("token");
+
+    if (token !== process.env.ADMIN_SETUP_TOKEN) {
+      return NextResponse.json({ error: "Non autorisé" }, { status: 401 });
+    }
     // 🔑 IDENTIFIANTS À PERSONNALISER
     const email = process.env.AUTHENTIK_ADMIN_EMAIL;
     const password = process.env.AUTHENTIK_ADMIN_PASSWORD;
